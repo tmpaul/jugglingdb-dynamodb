ac19b0052575c5fe4bfbad7e7ce43c2c6623cc70
tmpaul06 <tmpaul06@gmail.com>
2014-06-05 19:13:36 +0530

    Logging consumed capacity units

--- test/dynamo.test.js
+++ test/dynamo.test.js
@@ -7,9 +7,10 @@ describe('dynamodb', function(){
     before(function() {
       db = getSchema();
       User = db.define('User', {
-        id: { type: String, keyType: "hash", uuid: true},
+        id: { type: String, keyType: "hash"},
         name: { type: String },
         email: { type: String },
+        dob : { type: Date},
         age: {type: Number},
         tasks: { type: String, properties: { breaker: 2} }
       });
@@ -25,6 +26,7 @@ describe('dynamodb', function(){
 
   it('should create user without any errors', function (done) {
     var tempUser = new User({
+      id : "1",
       name: "John Doe",
       email: "john@doe.com",
       age: 20,
@@ -32,6 +34,7 @@ describe('dynamodb', function(){
     });
     User.create(tempUser, function (err, user) {
       should.not.exist(err);
+      user.should.have.property('id'); // Check for UUID generation
       user.should.have.property('name', 'John Doe');
       user.should.have.property('tasks');
       // Make sure that chunked data is not sent back to
@@ -48,14 +51,37 @@ describe('dynamodb', function(){
    */
   it('should handle undefined and null attributes and return the same from database', function(done){
      var tempUser = new User({
+      id : "2",
       email: null,
-      age: 20,
+      age: null,
       tasks: "Blah blah blah"
     });
     User.create(tempUser, function (err, user) {
       should.not.exist(err);
+      (user.dob === undefined).should.be.true;
+      (user.age === null).should.be.true;
       (user.email === null).should.be.true;
       done();
     });
   });
+
+  it('should return error saying hash key cannot be null', function(done){
+     var tempUser = new User({
+      id: null,
+      email: null,
+      age: null,
+      tasks: "Blah blah blah"
+    });
+    User.create(tempUser, function (err, user) {
+      should.exist(err);
+      done();
+    });
+  });
+  /*it('should handle search queries of `all` method', function(done){
+    User.all({ where : { age : { gt: 10} }}, function (err, users) {
+      should.not.exist(err);
+      (users.should.be.instanceof(Array).and.have.lengthOf(1);
+      done();
+    });
+  });*/
 });
