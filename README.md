d5d17527d7a8e72feabb69bc4124eb0bd86301b3
tmpaul06 <tmpaul06@gmail.com>
2014-06-11 19:39:04 +0530

    Added logging and cascade delete

--- README.md
+++ README.md
@@ -83,24 +83,24 @@
 - DynamoDB stores information in the form of tables. Each table has a collection of items. Think of items as a row in a table. Each
 item in turn is a collection of attributes. Each attribute is stored in the database as a key:value pair.
 
-######Key Schema
+#####Key Schema
 - DynamoDB primary key is a composite key consiting of a hash key and a range key. For a given item in the table, the key schema can specify
 one hash key or one hash key and a range key.
 - To specify a given attribute as hash key, use `keyType: "hash"` along with its definition as shown in the above example.
 - DynamoDB expects a hash key for every item that is created. If this value is not available at the time of creation, there is an option to use a UUID generator to generate the hash key. To use this option, specify `uuid: true` in the model definition.
 - Similarly, attribute becomes a range key if keyType is set to "range".
 
-######Data Types
+#####Data Types
 - DynamoDB supports only String, Binary, and Number datatypes along with their corresponding Sets.
 - The adapter currently supports String, Number, Date and Boolean datatypes.
 - Date is stored internally as a number, and boolean is stored as a string => `true` or `false`.
 
-######Read Write / Capacity Units
+#####Read Write / Capacity Units
 - Provisioned Throughput for each table can be specified by using the `set` property function of Jugglingdb. In the above example, the
 read and write capacity units are set for `User`.
 - The defaults for read and write capacity units are 5 and 10 respectively.
 
-######Limits
+#####Limits
 - DynamoDB has an item size limit of 64 kb. Typically data is stored in the form of big strings in NoSQL tables (e.g objects with complex data structures). Eventually these strings might exceed 64 kb in size. To overcome this limitation, the adapter uses a sharding technique. 
 - When an attribute is being sharded, the attribute is separated from the parent table, and is stored in a different (child) table. The attribute value (String) is broken down based on the size specified by the `splitter` attribute. For example, in the above given model definition, the tasks string is broken down into 60 kb chunks. Each chunk is stored as a new item and is then given a range key in the new table. The primary key of the parent item is also stored in this item.
 - When the main item is being retrieved from the database, the adapter queries each child table, and builds back the string. As a result, the data structure is still intact after retrieval.
--- test/dynamo.test.js
+++ test/dynamo.test.js
@@ -4,7 +4,7 @@ var User;
 
 describe('dynamodb', function(){
 
-    before(function() {
+    before(function(done) {
       db = getSchema();
       User = db.define('User', {
         id: { type: String, keyType: "hash"},
@@ -12,19 +12,30 @@ describe('dynamodb', function(){
         email: { type: String },
         dob : { type: Date},
         age: {type: Number},
-        tasks: { type: String, properties: { breaker: 2} }
+        tasks: { type: String, properties: { sharding : true, splitter : "10kb"} }
       });
+
+      Book = db.define('Book', {
+        id : { type: String, keyType: "hash"},
+        subject : { type: String, keyType: "range"},
+      });
+
+      setTimeout(function(){
+        done();
+      },1500);
+
     });
 
   beforeEach(function(done) {
     // Table creation is async. Wait 1.5s so that
     // table gets created.
-    setTimeout(function() {
-      done();
-    }, 1500);
+   
+    console.log("------------------------------------------------------");
+    done();
   });
 
   it('should create user without any errors', function (done) {
+    console.log('should create user without any errors');
     var tempUser = new User({
       id : "1",
       name: "John Doe",
@@ -34,7 +45,7 @@ describe('dynamodb', function(){
     });
     User.create(tempUser, function (err, user) {
       should.not.exist(err);
-      user.should.have.property('id'); // Check for UUID generation
+      user.should.have.property('id');
       user.should.have.property('name', 'John Doe');
       user.should.have.property('tasks');
       // Make sure that chunked data is not sent back to
@@ -50,6 +61,7 @@ describe('dynamodb', function(){
     to throw an error here.
    */
   it('should handle undefined and null attributes and return the same from database', function(done){
+    console.log('should handle undefined and null attributes and return the same from database');
      var tempUser = new User({
       id : "2",
       email: null,
@@ -66,6 +78,7 @@ describe('dynamodb', function(){
   });
 
   it('should return error saying hash key cannot be null', function(done){
+    console.log('should return error saying hash key cannot be null');
      var tempUser = new User({
       id: null,
       email: null,
@@ -77,11 +90,32 @@ describe('dynamodb', function(){
       done();
     });
   });
-  /*it('should handle search queries of `all` method', function(done){
-    User.all({ where : { age : { gt: 10} }}, function (err, users) {
-      should.not.exist(err);
-      (users.should.be.instanceof(Array).and.have.lengthOf(1);
-      done();
+
+  it('should create two books for same id but different subjects', function (done) {
+    console.log('should create two books for same id but different subjects');
+    var book1 = new Book({
+      id: "abcd",
+      subject: "Nature"
     });
-  });*/
+
+    var book2 = new Book({
+      id: "abcd",
+      subject: "Fiction"
+    });
+
+    Book.create(book1, function (err, _book1) {
+      should.not.exist(err);
+      should.exist(_book1);
+      _book1.should.have.property('id', 'abcd');
+      _book1.should.have.property('subject', 'Nature');
+
+      Book.create(book2, function (err, _book2) {
+        should.not.exist(err);
+        should.exist(_book2);
+        _book2.should.have.property('id', 'abcd');
+        _book2.should.have.property('subject', 'Fiction');
+        done();
+      });
+    });
+  });
 });
